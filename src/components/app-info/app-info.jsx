6fa9e1997cf7f925b652d106af096c7ce921e715
makrenka <smokrenko79@gmail.com>
2023-03-03 12:47:27 +0300

    add rise state

--- src/components/app-info/app-info.jsx
+++ src/components/app-info/app-info.jsx
@@ -1,9 +1,9 @@
 import './app-info.css';
 
-export const AppInfo = () => (
+export const AppInfo = ({ total, increase }) => (
   <div className="app-info">
     <h1>Учет сотрудников в компании</h1>
-    <h2>Общее число сотрудников:</h2>
-    <h2>Премию получат:</h2>
+    <h2>Общее число сотрудников: {total}</h2>
+    <h2>Премию получат: {increase}</h2>
   </div>
 );
